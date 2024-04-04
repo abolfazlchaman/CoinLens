fa467e617595de1d6b677821f1b5098408242c93
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2024-04-05 01:13:05 +0330

    fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       )
       .then((res) => {
         setCoins(res.data);
-        console.log(res.data);
+        // console.log(res.data);
       })
       .catch((error) => console.log(error));
   }, []);
